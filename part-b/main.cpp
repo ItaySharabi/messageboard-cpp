3e7744ade6d64ed04ba63e7dd596d596db75df3b
itay <itay.nextweb@gmail.com>
2021-04-06 21:25:50 +0300

    part-b implementation

--- part-b/main.cpp
+++ part-b/main.cpp
@@ -9,6 +9,9 @@ int main() {
 
     Board board;
 
+
+    board.post(0, 0, Direction::Horizontal, "Hello");
+
     board.show();
 
     board.post(2, 4, Direction::Vertical, "Hello");
@@ -20,33 +23,6 @@ int main() {
     board.post(10, 10, Direction::Horizontal, "Good Bye!");
 
     board.show();
-
-
-    board.post(5, 5, Direction::Horizontal, "And");
-
-    board.show();
-
-    // board.post(100, 5, Direction::Horizontal, "KLKLK");
-
-    // board.show();
-
-    // board.post(27, 34, Direction::Horizontal, "Hello There!");
-
-    // board.show();
-    
-    board.post(1, 50, Direction::Horizontal, "ASDAUWGF");
-    board.show();
-
-
-    board.post(6, 25, Direction::Horizontal, "Doing well so far!");
-    board.show();
-    // for(int i = 1; i <= 5; i++) {
-
-    //     srand(clock());
-    //     unsigned int d = rand()%2;
-    //     board.post(i, i+50, (d%2 == 0 ? Direction::Horizontal : Direction::Vertical), "KAKA");
-    //     board.show();
-    // }
 
     return 0;
 }
