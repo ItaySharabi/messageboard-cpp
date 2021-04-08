506eaf5cd9d3e639b8263b0664cf4bfd2ad2f638
itay <itay.nextweb@gmail.com>
2021-04-08 06:44:30 +0300

    Ready to present main, Add documentation though

--- main.cpp
+++ main.cpp
@@ -38,32 +38,33 @@ int main() {
     const Direction horiz = Direction::Horizontal;
     const Direction vert = Direction::Vertical;
     int exit = 0;
-    string s;
     
         
     cout << "\n\nWelcome to my Dynamic Messageboard!" << endl;
 
     cout << "You can choose input from 1 - 5 and practice with the messageboard:" << endl;
 
-    unsigned int user_choice = 0;
+    unsigned int user_choice = 1;
 
     unsigned int i, j;
     int d;
     Direction direction;
 
-    while(!exit) {
+    while(!exit && user_choice) {
         board.show();
+        cout << "----------------------------------------------------------------\n";
         cout << "1: post(i, j, Direction, Text)" << endl;    
         cout << "2: read(i, j, (Horizontal=0, Vertical=1), Length) - Get text from (i,j), 'Length' chars long" << endl;    
         cout << "3: random snowman - post a random snowman" << endl;    
         cout << "4: custom snowman(8 digits from 1-4) - post a snowman ;)" << endl; 
         cout << "5: exit - Stop the program" << endl;
+        cout << "----------------------------------------------------------------\n";
         cin >> user_choice;
         switch(user_choice) {
             case 1:
-                cout << "i = ";
+                cout << "set i = ";
                 cin >> i;
-                cout << "j = ";
+                cout << "set j = ";
                 cin >> j;
                 cout << "Direction: 1 - Horizontal\n\t   2 - Vertical" << endl;
                 cin >> d;
@@ -81,11 +82,12 @@ int main() {
                 getchar();
                 cin.getline(message, MAX_LINE);
                 board.post(i, j, direction, message);
+                cout << "\n\n";
                 break;
             
             case 2:
                 cout << "Read text from the messageboard: " << endl;
-                cout << "i = "; cin >> i; cout << "j = ";
+                cout << "set i = "; cin >> i; cout << "set j = "; cin >> j;
                 cout << "Direction: 1 - Horizontal\n\t   2 - Vertical" << endl;
                 cin >> d;
                 switch(d) {
@@ -100,7 +102,7 @@ int main() {
                 cout << "Reading length: "; cin >> length;
                 try {
                     string out = board.read(i, j, direction, length);
-                    cout << out;
+                    cout << "--------\nContent: \n" << out << "\n\n--------" << endl;;
                 } catch (exception e) {
                     cout << e.what();
                 }
@@ -110,8 +112,8 @@ int main() {
                 cout << "Random snowman at your request! Shown at (0,0)" << endl;
                 try {
                     int k = rand_snowman_input();
-                    cout << k << endl;
                     board.post(0, 0, Direction::Horizontal, snowman(k));
+                    cout << "\n\n";
                 } catch (exception e) {
                     cout << e.what();
                 }
@@ -121,12 +123,13 @@ int main() {
                 cout << "Enter 8 digits in range 1-4" << endl;
                 cin >> snow_input;
                 cout << "Where do u wanna put it? (i, 1)" << endl;
-                cout << "i = ";cin >> i;
+                cout << "set i = ";cin >> i;
                 try {
                     board.post(i, 1, horiz, snowman(snow_input));
                 } catch(exception e) {
                     cout << e.what() << endl;
                 }
+                cout << "\n\n";
                 continue;
             case 5:
                 cout << "Quitting" << endl;
